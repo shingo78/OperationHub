dd799c0379bea8d5e359abc67f96167f5090fe3a
Shingo Nakagawa <shingo.nakagawa@gmail.com>
2024-05-16 15:54:43 +0900

    Fix install-host-services.sh

--- install-host-services.sh
+++ install-host-services.sh
@@ -12,7 +12,7 @@ if printf '%s\n' $(echo ${ID_LIKE[@]}) | grep -qx "rhel"; then
     pip3 install tornado
 elif [ $ID == "ubuntu" ]; then
     apt-get update
-    apt-get install -y python3 python3-dev python3-pip
+    apt-get install -y python3 python3-dev python3-pip inotify-tools
 else
     exit 1
 fi
